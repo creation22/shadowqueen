42376e85543936ddfe3dc0cc867eff5d113d34ec
creation <creation2224@gmail.com>
2025-10-01 23:36:38 +0530

    error fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navabar from './Navabar'
 import Background from './Background'
-import { AnimatedShinyText } from "./ui/animated-shiny-text"
 
 const Home = () => {
     return (
